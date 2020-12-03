01d4ba63c95f6623ffb1fcaaed538c0e3e2dbc04
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2020-12-03 17:24:40 +0100

    Add decorator factory for memory utilization maximization

--- src/tools.py
+++ src/tools.py
@@ -1,10 +1,14 @@
 import functools
+import inspect
 import itertools
+import logging
 import operator
-from typing import Callable, SupportsFloat, Tuple, Union
+from typing import Callable, SupportsFloat, Tuple, TypeVar, Union
 
 import numpy
 import torch
+
+logger = logging.getLogger(__name__)
 
 
 def calculate_broadcasted_elementwise_result_shape(
@@ -117,3 +121,119 @@ def tensor_sum(*tensors: torch.FloatTensor) -> torch.FloatTensor:
         sum(*tensors) evaluated in an optimal processing order.
     """
     return _multi_combine(tensors=tensors, op=operator.add)
+
+
+def _is_oom_error(error: RuntimeError) -> bool:
+    """Check whether a runtime error was caused by insufficient memory."""
+    message = error.args[0]
+    logger.debug(f"Checking error for OOM: {message}")
+
+    # CUDA out of memory
+    if 'CUDA out of memory.' in message:
+        return True
+
+    # CUDA error (dimension was larger than int limit)
+    if "RuntimeError: CUDA error: invalid configuration argument" in message:
+        return True
+
+    # CPU out of memory
+    if "[enforce fail at CPUAllocator.cpp:64] . DefaultCPUAllocator: can't allocate memory:" in message:
+        return True
+
+    return False
+
+
+R = TypeVar("R")
+
+
+def maximize_memory_utilization_(
+    parameter_name: str,
+    q: int = 32,
+    cpu_warning: bool = True,
+) -> Callable[[Callable[..., R]], Callable[..., Tuple[R, int]]]:
+    """
+    A decorator factory to create methods for memory utilization maximization.
+
+    :param parameter_name:
+        The parameter name.
+    :param q:
+        Prefer multiples of q as size.
+    :param cpu_warning:
+        Whether to check the input for CPU tensors and warn about potential CPU OOM problems.
+
+    :return:
+        A decorator for functions.
+    """
+
+    if cpu_warning:
+        def check_for_cpu_tensors(*args, **kwargs):
+            if any(
+                (torch.is_tensor(obj) and obj.device.type == "cpu")
+                for obj in itertools.chain(args, kwargs.values())
+            ):
+                logger.warning(
+                    "Using maximize_memory_utilization on non-CUDA tensors. This may lead to undocumented crashes due to CPU OOM killer."
+                )
+    else:
+        def check_for_cpu_tensors(*args, **kwargs):
+            pass
+
+    def decorator_maximize_memory_utilization(func: Callable[..., R]) -> Callable[..., Tuple[R, int]]:
+        """
+        A decorator to maximize memory utilization.
+
+        :param func:
+            The function to decorate.
+
+        :return:
+            The decorated function.
+        """
+        # Input validation
+        signature = inspect.signature(func)
+        if parameter_name not in signature.parameters.keys():
+            raise ValueError(f"{func} does not have a parameter {parameter_name}.")
+        _parameter = signature.parameters[parameter_name]
+        if _parameter.kind != inspect.Parameter.POSITIONAL_OR_KEYWORD:
+            # TODO: we could also support positional ones by saving the position
+            raise ValueError(f"{parameter_name} must be a keyword based parameter.")
+        if _parameter.annotation != inspect.Parameter.empty and _parameter.annotation != int:
+            logger.warning(f"Memory utilization maximization is written for integer parameters, but the {parameter_name} is annotated as {_parameter.annotation}")
+
+        @functools.wraps(func)
+        def wrapper_maximize_memory_utilization(*args, **kwargs) -> Tuple[R, int]:
+            """
+            A wrapper around the function to maximize memory utilization by successive halving.
+
+            :param args:
+                The positional arguments.
+            :param kwargs:
+                The key-word based arguments.
+
+            :return:
+                A tuple (result, max_value).
+            """
+            check_for_cpu_tensors(*args, **kwargs)
+            max_value = kwargs.pop(parameter_name)
+            while max_value > 0:
+                p_kwargs = {
+                    parameter_name: max_value
+                }
+                try:
+                    return func(*args, **p_kwargs, **kwargs), max_value
+                except RuntimeError as runtime_error:
+                    # clear cache
+                    torch.cuda.empty_cache()
+
+                    # check whether the error is an out-of-memory error
+                    if not _is_oom_error(error=runtime_error):
+                        raise runtime_error
+
+                    logger.info(f"Execution failed with {parameter_name}={max_value}")
+                    max_value //= 2
+                    if max_value > q:
+                        max_value = max_value // q * q
+            raise MemoryError(f"Execution did not even succeed with {parameter_name}=1.")
+
+        return wrapper_maximize_memory_utilization
+
+    return decorator_maximize_memory_utilization
